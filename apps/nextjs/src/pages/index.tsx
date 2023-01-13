8362a3ec3c174a311e7db7df30d6e739f2807b12
zbndm <zbndmt@gmail.com>
2023-01-13 20:57:52 +0200

    Update index.tsx
    7

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
       <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> Turbo
+            Create <span className="text-[hsl(280,100%,70%)]">T9993</span> Turbo
           </h1>
           <AuthShowcase />
 
